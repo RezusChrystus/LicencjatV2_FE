aee9b1aa466a5e8d5b01f1645c8a4d5392ce8ed0
Filip Manowski <filipmanowski@gmail.com>
2025-06-09 23:14:30 +0200

    works!

--- src/main/java/org/example/licencjatv2_fe/EntryWindow/EntryController.java
+++ src/main/java/org/example/licencjatv2_fe/EntryWindow/EntryController.java
@@ -4,10 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import org.example.licencjatv2_fe.Api.ApiClient;
 import org.example.licencjatv2_fe.Classes.User;
@@ -23,7 +20,7 @@ public class EntryController {
     @FXML
     private TextField loginTextField;
     @FXML
-    private TextField passwordTextField;
+    private PasswordField passwordTextField;
     @FXML
     private TextField confirmPasswordTextField = new TextField();
     @FXML
